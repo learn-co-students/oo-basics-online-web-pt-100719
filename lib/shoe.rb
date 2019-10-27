7f28c453143be0f48c6d60c15f44b4a9993eba9b
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-27 07:39:16 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ def initialize(brand)
   end
   attr_accessor :brand, :color, :size, :material, :condition
   
-  def cobble(condition)
+  def cobble(condition = "new")
     @condition = condition
     puts "Your shoe is as good as new!"
   end
